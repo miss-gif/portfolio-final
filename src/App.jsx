7a0a0ad5030c27fae34dc8fbead1f58c84790a50
miss-gif <ddc.akari@gmail.com>
2024-07-24 22:40:52 +0900

    Add : 기본 구조

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import React from "react";
+import Home from "./components/home";
+import Resume from "./components/resume";
+import Portfolio from "./components/portfolio";
+import Services from "./components/services";
+import Skills from "./components/skills";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <header>헤더</header>
+      <main>
+        <Home />
+        <Portfolio />
+        <Resume />
+        <Services />
+        <Skills />
+      </main>
+      <footer>푸터</footer>
+    </div>
+  );
 };
 
 export default App;
