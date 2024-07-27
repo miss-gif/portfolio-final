55a90bcee07b0dadbd9488e1a4cbb6a85d7f5a1f
miss-gif <ddc.akari@gmail.com>
2024-07-27 19:48:13 +0900

    Add : 더미 텍스트 추가

--- src/data/data.js
+++ src/data/data.js
@@ -38,37 +38,38 @@ export const services = [
     id: 1,
     name: "Web Development",
     title: "Web Design",
-    description: "웹사이트 개발을 위한 레이아웃, 디자인 작업",
+    description: "반응형 디자인, 웹 접근성, 웹 표준을 고려한 UI/UX 개발 경험",
   },
   {
     id: 1,
     name: "Web Development",
     title: "Web Design",
-    description: "웹사이트 개발을 위한 레이아웃, 디자인 작업",
+    description:
+      "요구사항 분석 및 다른 개발자와 협업을 위한 원활한 커뮤니케이션 능력",
   },
   {
     id: 1,
     name: "Web Development",
     title: "Web Design",
-    description: "웹사이트 개발을 위한 레이아웃, 디자인 작업",
+    description: "성능 이슈, 메모리 누수, 네트워크 프로파일링 경험",
   },
   {
     id: 1,
     name: "Web Development",
     title: "Web Design",
-    description: "웹사이트 개발을 위한 레이아웃, 디자인 작업",
+    description: "신규 서비스 플랫폼 개발 및 기존 서비스 고도화",
   },
   {
     id: 1,
     name: "Web Development",
     title: "Web Design",
-    description: "웹사이트 개발을 위한 레이아웃, 디자인 작업",
+    description: "더 나은 고객 경험 제공을 위한 서비스 개선",
   },
   {
     id: 1,
     name: "Web Development",
     title: "Web Design",
-    description: "웹사이트 개발을 위한 레이아웃, 디자인 작업",
+    description: "기술 부채 식별 및 점진적 개선",
   },
 ];
 
