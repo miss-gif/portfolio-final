d82a5308fb6d039a1bf2b5de60889cdcce265836
miss-gif <ddc.akari@gmail.com>
2024-08-01 22:25:42 +0900

    Refactor : 타입스크립트로 마이그레이션

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+import Modal from "react-modal";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 1000, // 애니메이션 지속 시간 (밀리초)
+  once: false,
+});
+
+Modal.setAppElement("#root");
+
+// 타입 단언(Type Assertion)을 사용하여 'root' 요소를 HTMLDivElement로 지정
+const rootElement = document.getElementById("root") as HTMLElement;
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+} else {
+  console.error("Root element not found");
+}
