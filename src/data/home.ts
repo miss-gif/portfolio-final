931900b3356ae2b8597a7df4da5a176c77487150
miss-gif <ddc.akari@gmail.com>
2024-08-01 23:01:39 +0900

    Refactor : 타입스크립트로 마이그레이션

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,4 +1,11 @@
-export const home = {
+// 객체 타입 정의
+interface HomeInfo {
+  name: string;
+  description: string;
+}
+
+// home 객체 정의
+export const home: HomeInfo = {
   name: "곽도억",
   description: `웹 개발은 끊임없이 변화하지만, 저는 기술적인 숙련도뿐 아니라 사용자
     중심의 사고를 중요하게 생각합니다. 기능성과 심미성을 갖춘 웹사이트
--- src/utils/scrollClick.ts
+++ src/utils/scrollClick.ts
@@ -1,6 +1,9 @@
 const headerHeight = 100; // 헤더 높이
 
-export const handleClick = (event, section) => {
+export const handleClick = (
+  event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  section: string
+): void => {
   event.preventDefault();
   const element = document.getElementById(section);
   if (element) {
