40b13ecd211ba7029ea5207ba017db2bbfa93bac
miss-gif <ddc.akari@gmail.com>
2024-08-04 05:56:04 +0900

    Fix : 스킬 섹션 데이터 처리 비동기 방식으로 수정

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,9 +1,10 @@
-import { skills } from "./skills";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./skills.scss";
 
 // 객체 타입 정의
 interface Skills {
-  img: string | null;
+  img: string;
   id: number;
   name: string;
   percentage: number;
@@ -11,6 +12,24 @@ interface Skills {
 }
 
 const Skills = () => {
+  const [skills, setSkills] = useState<Skills[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSkills = async () => {
+      try {
+        const response = await axios.get("./json/skills.json");
+        setSkills(response.data);
+      } catch (error) {
+        console.error("Error fetching skills data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchSkills();
+  }, []);
+
   return (
     <section className="skills section">
       <h2 className="section__title text-cs" id="skills">
@@ -20,42 +39,49 @@ const Skills = () => {
         My <span>기술스택</span>
       </p>
       <div className="skills__container container grid">
-        {skills.map(({ img, name, percentage, description }, id) => {
-          return (
-            <ul>
-              <li
-                className="skills__item"
-                key={id}
-                data-aos="fade-up"
-                data-aos-duration="1500"
-              >
-                <div className="skills__item-img__cover">
-                  <img src={img} alt={img} />
-                </div>
-                <div className="skills__item__info">
-                  <div className="skills__titles">
-                    <h3 className="skills__name">{name}</h3>
-                    <span className="skills__number">
-                      {percentage} <span>%</span>
-                    </span>
-                  </div>
-                  <p className="skills__description">{description}</p>
-                  <div className="skills__bar">
-                    <span
-                      className="skills__percentage"
-                      style={{ width: `${percentage}%` }}
-                    >
-                      <span></span>
-                    </span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          );
-        })}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          skills.map(({ img, name, percentage, description, id }) => (
+            <SkillItem
+              key={id}
+              img={img}
+              name={name}
+              percentage={percentage}
+              description={description}
+            />
+          ))
+        )}
       </div>
     </section>
   );
 };
+
+const SkillItem = ({ img, name, percentage, description }) => (
+  <ul>
+    <li className="skills__item" data-aos="fade-up" data-aos-duration="1500">
+      <div className="skills__item-img__cover">
+        <img src={img} alt={name} />
+      </div>
+      <div className="skills__item__info">
+        <div className="skills__titles">
+          <h3 className="skills__name">{name}</h3>
+          <span className="skills__number">
+            {percentage} <span>%</span>
+          </span>
+        </div>
+        <p className="skills__description">{description}</p>
+        <div className="skills__bar">
+          <span
+            className="skills__percentage"
+            style={{ width: `${percentage}%` }}
+          >
+            <span></span>
+          </span>
+        </div>
+      </div>
+    </li>
+  </ul>
+);
 
 export default Skills;
