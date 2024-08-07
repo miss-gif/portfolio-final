fe4e2ef44b16b74eaf257e60c66451068a63550e
miss-gif <svx327@naver.com>
2024-08-07 19:43:30 +0900

    Test : 메일 전송 기능

--- src/components/contact/ContactContent.jsx
+++ src/components/contact/ContactContent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./ContactForm.scss";
+import "./ContactContent.scss";
 
 const ContactContent = () => {
   const [formData, setFormData] = useState({
@@ -7,6 +7,7 @@ const ContactContent = () => {
     email: "",
     message: "",
   });
+  const [complete, setComplete] = useState(false);
 
   const contactInfo = [
     { icon: "📍", label: "Address", value: "대구" },
@@ -22,7 +23,28 @@ const ContactContent = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    const form = e.target;
+
+    fetch(form.action, {
+      method: form.method,
+      body: new FormData(form),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.result === "success") {
+          setComplete(true);
+          setFormData({
+            name: "",
+            email: "",
+            message: "",
+          });
+        } else {
+          console.error("Form submission error:", response);
+        }
+      })
+      .catch((error) => {
+        console.error("Form submission failed:", error);
+      });
   };
 
   return (
@@ -42,60 +64,84 @@ const ContactContent = () => {
       </div>
 
       <div className="contact-content__form">
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <div className="contact-form__elements">
-            <fieldset className="contact-form__group">
-              <label htmlFor="name" className="contact-form__label">
-                이름
-              </label>
-              <input
-                id="name"
-                name="name"
-                className="contact-form__input"
-                required
-                placeholder="귀하의 성함을 적어주세요."
-                value={formData.name}
-                onChange={handleChange}
-              />
-            </fieldset>
+        {!complete ? (
+          <form
+            className="contact-form"
+            method="POST"
+            action="https://script.google.com/macros/s/AKfycbyeWNQ90Hjbb1Pd7iKaRdHbHY1wVfNO7v_0g_Ky-cK60d6HkKXf1d7If58fPVg9rwp_ZA/exec"
+            onSubmit={handleSubmit}
+          >
+            <div className="contact-form__elements">
+              <fieldset className="contact-form__group">
+                <label htmlFor="name" className="contact-form__label">
+                  이름
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  className="contact-form__input"
+                  required
+                  placeholder="귀하의 성함을 적어주세요."
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </fieldset>
 
-            <fieldset className="contact-form__group">
-              <label htmlFor="email" className="contact-form__label">
-                이메일
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="contact-form__input"
-                required
-                placeholder="답변받으실 이메일 주소를 적어주세요."
-                value={formData.email}
-                onChange={handleChange}
-              />
-            </fieldset>
+              <fieldset className="contact-form__group">
+                <label htmlFor="email" className="contact-form__label">
+                  이메일
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="contact-form__input"
+                  required
+                  placeholder="답변받으실 이메일 주소를 적어주세요."
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </fieldset>
 
-            <fieldset className="contact-form__group">
-              <label htmlFor="message" className="contact-form__label">
-                메세지
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                rows="5"
-                className="contact-form__textarea"
-                required
-                placeholder="궁금하신 점이 있으시면 언제든지 문의해 주세요."
-                value={formData.message}
-                onChange={handleChange}
-              />
-            </fieldset>
+              <fieldset className="contact-form__group">
+                <label htmlFor="message" className="contact-form__label">
+                  내용
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="5"
+                  className="contact-form__textarea"
+                  required
+                  placeholder="궁금하신 점이 있으시면 언제든지 문의해 주세요."
+                  value={formData.message}
+                  onChange={handleChange}
+                />
+              </fieldset>
 
-            <button type="submit" className="contact-form__submit-button">
-              Send
-            </button>
+              <fieldset
+                className="contact-form__group honeypot-field"
+                style={{ display: "none" }}
+              >
+                <label htmlFor="honeypot">
+                  스팸 방지를 위해 빈 칸으로 제출해야 하는 숨겨진 필드입니다.
+                  작성하지 마세요!
+                </label>
+                <input id="honeypot" type="text" name="honeypot" value="" />
+              </fieldset>
+
+              <button type="submit" className="contact-form__submit-button">
+                보내기
+              </button>
+            </div>
+          </form>
+        ) : (
+          <div className="thankyou_message">
+            <h2>
+              <em>감사합니다</em> 연락주셔서 감사합니다! 곧 답변드리겠습니다.
+            </h2>
           </div>
-        </form>
+        )}
       </div>
     </div>
   );
