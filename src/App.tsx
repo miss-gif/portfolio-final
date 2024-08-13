0b97b1783f1f8e52a1babc52ab0d1a0b6937c341
miss-gif <ddc.akari@gmail.com>
2024-08-13 22:25:44 +0900

    Add : 인증 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import Cv from "./components/cv/index";
+import AuthLayout from "./components/layout/AuthLayout";
 import RootLayout from "./components/layout/RootLayout";
+import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import Test from "./pages/Test";
+import Profile from "./components/Profile/Profile.jsx";
+import ProfileEdit from "./components/Profile/ProfileEdit.jsx";
 
 const App = () => {
   return (
     <Routes>
+      {/* 인증 페이지 */}
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route index element={<AuthPage />} />
+      </Route>
+
+      {/* 소개 페이지 */}
       <Route path="/" element={<RootLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="profile-edit" element={<ProfileEdit />} />
       </Route>
+
+      {/* 서브 링크 */}
       <Route path="/cv" element={<Cv />} />
       <Route path="/test" element={<Test />} />
       <Route path="*" element={<NotFound />} />
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const AuthPage = () => {
+  return (
+    <>
+      <main>
+        <div>123</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+        <div>dd</div>
+      </main>
+    </>
+  );
+};
+
+export default AuthPage;
