12d4990ad3dcdded1feb9ba0f93b1b78fc5319ef
miss-gif <ddc.akari@gmail.com>
2024-09-06 01:12:46 +0900

    Test : 더미데이터 추가 기능

--- src/components/admin/PostManager.jsx
+++ src/components/admin/PostManager.jsx
@@ -1,20 +1,16 @@
 // src/BoardPage.js
-import React, { useState, useEffect } from "react";
+import { collection, doc, getDocs, writeBatch } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { dummy } from "../../api/dummy"; // dummy 데이터를 가져옵니다.
 import { db } from "../../firebaseConfig";
-import {
-  collection,
-  addDoc,
-  getDocs,
-  deleteDoc,
-  doc,
-  writeBatch,
-} from "firebase/firestore";
-import { useStore } from "../../store/store";
-import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const PostManager = () => {
   const [posts, setPosts] = useState([]);
-  const [newPost, setNewPost] = useState("");
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const fetchPosts = async () => {
     try {
@@ -29,30 +25,6 @@ const PostManager = () => {
     }
   };
 
-  const handleAddPost = async () => {
-    if (newPost.trim()) {
-      try {
-        await addDoc(collection(db, "posts"), {
-          content: newPost,
-          timestamp: new Date(),
-        });
-        setNewPost("");
-        fetchPosts();
-      } catch (error) {
-        console.error("게시물 추가 실패: ", error);
-      }
-    }
-  };
-
-  const handleDeletePost = async (postId) => {
-    try {
-      await deleteDoc(doc(db, "posts", postId));
-      fetchPosts();
-    } catch (error) {
-      console.error("게시물 삭제 실패: ", error);
-    }
-  };
-
   const handleDeleteAllPosts = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, "posts"));
@@ -64,32 +36,54 @@ const PostManager = () => {
 
       await batch.commit();
       fetchPosts();
+      toast.success("전체 게시물 삭제 성공");
     } catch (error) {
-      console.error("전체 게시물 삭제 실패: ", error);
+      toast.error("전체 게시물 삭제 실패");
+    }
+  };
+
+  const handleAddDummyPosts = async () => {
+    try {
+      const batch = writeBatch(db);
+      dummy.forEach((item) => {
+        const postData = {
+          author: item.author,
+          content: item.content,
+          date: item.date,
+          likes: item.likes,
+          postNumber: item.postId, // Firestore에서는 postNumber로 대체
+          title: item.title,
+          views: item.views,
+        };
+        // 문서 참조 생성
+        const postRef = doc(collection(db, "posts"));
+        batch.set(postRef, postData);
+      });
+      await batch.commit();
+      fetchPosts();
+      toast.success("더미 게시물 추가 성공");
+    } catch (error) {
+      toast.error("더미 게시물 추가 실패");
     }
   };
 
   return (
     <div>
       <h2>게시물 관리</h2>
-      <div>
-        <input
-          type="text"
-          value={newPost}
-          onChange={(e) => setNewPost(e.target.value)}
-          placeholder="새 게시물 내용"
-        />
-        <button onClick={handleAddPost}>추가</button>
+      <div className="flex gap-2">
+        <button
+          className="p-2 bg-green-800 text-white rounded"
+          onClick={handleDeleteAllPosts}
+        >
+          전체 삭제
+        </button>
+        <button
+          className="p-2 bg-green-800 text-white rounded"
+          onClick={handleAddDummyPosts}
+        >
+          더미 게시물 추가
+        </button>
       </div>
-      <div>
-        {posts.map((post) => (
-          <div key={post.id}>
-            <p>{post.content}</p>
-            <button onClick={() => handleDeletePost(post.id)}>삭제</button>
-          </div>
-        ))}
-      </div>
-      <button onClick={handleDeleteAllPosts}>전체 삭제</button>
     </div>
   );
 };
